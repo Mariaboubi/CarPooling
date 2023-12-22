708925213877803609ba6f8d612fe20a1dc10526
ina <inavrousai12@gmail.com>
2023-12-22 18:26:56 +0200

    sign up page

---
+++ app/src/main/java/gr/aueb/carpooling/Driver_Register.java
@@ -0,0 +1,14 @@
+package gr.aueb.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Driver_Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_register);
+    }
+}
---
+++ app/src/main/java/gr/aueb/carpooling/sign_up_choise_page.java
@@ -0,0 +1,14 @@
+package gr.aueb.carpooling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sign_up_choise_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_choise_page);
+    }
+}
