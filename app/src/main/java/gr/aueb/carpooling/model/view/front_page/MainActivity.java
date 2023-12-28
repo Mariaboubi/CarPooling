4c38b49632be18f148fbf1a809db131d2e5eef13
elenizanou <elzanou@gmail.com>
2023-12-28 21:11:46 +0200

    test

---
+++ app/src/main/java/gr/aueb/carpooling/model/view/front_page/MainActivity.java
@@ -0,0 +1,16 @@
+//package gr.aueb.carpooling.model.view.front_page;
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import gr.aueb.carpooling.R;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_log_in);
+//    }
+//}
