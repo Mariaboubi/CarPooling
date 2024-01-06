9281087ed1f3f845f5f0f3f1b08c4f92d85d9596
elenizanou <elzanou@gmail.com>
2024-01-06 17:23:52 +0200

    update passenger rating driver layout

--- app/src/main/java/gr/aueb/carpooling/model/view/passenger/DriverRating/DriverRatingActivity.java
+++ app/src/main/java/gr/aueb/carpooling/model/view/passenger/DriverRating/DriverRatingActivity.java
@@ -1,4 +1,4 @@
-package gr.aueb.carpooling.model.view.passenger.DriverRaiting;
+package gr.aueb.carpooling.model.view.passenger.DriverRating;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import gr.aueb.carpooling.R;
 
-public class DriverRaitingActivity extends AppCompatActivity {
+public class DriverRatingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_passenger_raiting_driver);
+        setContentView(R.layout.activity_passenger_rating_driver);
     }
 }
