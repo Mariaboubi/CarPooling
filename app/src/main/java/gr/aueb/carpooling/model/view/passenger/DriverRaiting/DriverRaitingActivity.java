230a1a0ca5694f82a7741be40283ab302b293562
Maria_3210135 <p3210135@aueb.gr>
2024-01-06 17:03:34 +0200

    DriverRaiting

---
+++ app/src/main/java/gr/aueb/carpooling/model/view/passenger/DriverRaiting/DriverRaitingActivity.java
@@ -0,0 +1,16 @@
+package gr.aueb.carpooling.model.view.passenger.DriverRaiting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import gr.aueb.carpooling.R;
+
+public class DriverRaitingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_raiting_driver);
+    }
+}
